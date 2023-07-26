cbef33c7f5646b16b48b4206826f6f38550b7d64
htram-dev <hoang.tram1881@gmail.com>
2023-07-26 18:50:49 -0500

    More code changes

--- lstm_backtest/quantile_value.py
+++ lstm_backtest/quantile_value.py
@@ -1,3 +1,4 @@
+from typing import List
 import pandas as pd
 from dataclasses import dataclass
 
@@ -13,9 +14,19 @@ class QuantileBand:
     upper_bound: QuantileValue
 
 
-def generate_quantile_bands(values: pd.Series) -> QuantileBand:
+def generate_quantile_bands(values: pd.Series) -> List[QuantileBand]:
   quantile_count = len(values) - 1
   return [QuantileBand(QuantileValue(values.index[i  ], values.iloc[i  ]), 
                        QuantileValue(values.index[i+1], values.iloc[i+1])) 
           for i in range(len(values)-1)][:quantile_count]
+
+
+
+def extract_boundary_values_from_quantile_bands(quantile_bands: List[QuantileBand]) -> List:
+  values = list(set([entry.lower_bound.value for entry in quantile_bands] + [entry.upper_bound.value for entry in quantile_bands]))
+
+  values.sort()
+
+  return values
+                                                
     
