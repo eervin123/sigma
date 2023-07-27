7d46ca0eac2075bce95fb66dc9e037769f7a5d17
htram-dev <hoang.tram1881@gmail.com>
2023-07-27 14:15:57 -0500

    More code changes

--- lstm_backtest/long_minus_short_backtests.py
+++ lstm_backtest/long_minus_short_backtests.py
@@ -100,3 +100,33 @@ def run_backtest_long_minus_short_entry_type_long_short(df: pd.DataFrame, result
           key = f"Long minus short with slopes"    
           slopes = PredictionWindowSlopes(long_slope_threshold, short_slope_threshold, None, None, threshold, EntryType.LONG_SHORT)
           store_backtest_results(key, pf, results_as_list, slopes)
+
+
+
+#run_backtest_long_minus_short_entry_type_long_short(df, results_as_list, prediction_window, threshold_increment, min_num_entries, quantiles)
+
+# loop 1 is split into 100
+# for threshold in long_minus_short_thresholds:  
+#   for long_slope_threshold in long_slope_quantile_values:
+#     for short_slope_threshold in short_slope_quantile_values:
+#       entries       = pd.Series(np.where((df['long_minus_short'] < threshold) & (df['long_slope'] > long_slope_threshold), True, False))
+#       short_entries = pd.Series(np.where((df['long_minus_short'] < threshold) & (df['short_slope'] < short_slope_threshold), True, False))
+
+#       num_entries = (entries == True).sum() + (short_entries == True).sum()
+
+#       if num_entries > min_num_entries:
+#         pf = vbt.Portfolio.from_signals(
+#             high              = df['BTCUSDT_High'],
+#             low               = df['BTCUSDT_Low'],
+#             open              = df['BTCUSDT_Open'],
+#             close             = df['BTCUSDT_Close'],
+#             entries           = entries, # commented out for a short only backtest    
+#             short_entries     = short_entries,
+#             td_stop           = prediction_window, # Hold on to the position for 8 bars
+#             time_delta_format = 'Rows', # Use the row index to calculate the time delta    
+#             accumulate        = False,    
+#             )
+
+#         key = f"Long minus short with slopes - type 3"    
+#         slopes = PredictionWindowSlopes(long_slope_threshold, short_slope_threshold, threshold, EntryType.LONG_SHORT)
+#         store_backtest_results(key, pf, results_as_list, slopes)
