e2717be0035cf1e52e4236f716b706379e830a14
htram-dev <hoang.tram1881@gmail.com>
2023-08-27 17:08:43 -0500

    Added code to combine btc prices

--- backtest_script.py
+++ backtest_script.py
@@ -7,10 +7,14 @@ from backtesting import Strategy, Backtest
 DEFAULT_CHUNK_SIZE    = 5000000         # The price file will be very large - so we will process it in chunks
 NO_CHUNK_SIZE_VALUE   = -1              # Use this value to process the entire file in one chunk
 NO_CHUNK_INDEX_VALUE  = "NoChunk"
-DEFAULT_PRICE_FILE    = "secbtcusdtm_20192020.csv"
-DEFAULT_TRADE_FILE    = "signal - yosemite btc - slow.csv"
 DATA_DIR              = "data"  
 RESULTS_DIR           = "results"     
+
+BTC_PRICE_FILE_PART1  = "secbtcusdtm_20192020.csv"
+BTC_PRICE_FILE_PART2  = "btcsec2021-23.csv"
+ALL_BTC_PRICE_FILES   = "all_btc_prices"
+DEFAULT_PRICE_FILE    = BTC_PRICE_FILE_PART1
+DEFAULT_TRADE_FILE    = "signal - yosemite btc - slow.csv"
 
 NUM_PRICES_PER_HOUR   = 60 * 60         # When using minute data, it should be 60.  Seconds data should be 60 x 60
 
@@ -178,12 +182,45 @@ def _process_one_chunk(chunk_df: pd.DataFrame, price_file: str, trade_file: str,
 
 
 
+def _combine_and_convert_all_btc_prices() -> pd.DataFrame:
+  btc_df1 = pd.read_csv(os.path.join(os.getcwd(), DATA_DIR, BTC_PRICE_FILE_PART1))
+  btc_df1 = _convert_chunk_df_to_correct_format(btc_df1, _extract_file_name_no_ext(BTC_PRICE_FILE_PART1))
 
-def _process_backtest(price_file: str, trade_file: str, chunk_size: int):
-  print(f'Processing backtest using price file "{price_file}" and trade file "{trade_file}"...')
+  btc_df2 = pd.read_csv(os.path.join(os.getcwd(), DATA_DIR, BTC_PRICE_FILE_PART2))
+  btc_df2 = _convert_chunk_df_to_correct_format(btc_df2, _extract_file_name_no_ext(BTC_PRICE_FILE_PART2))
+
+  combined_df = pd.concat([btc_df1, btc_df2])
+  combined_df.sort_index(inplace=True)
+
+  return combined_df
+
+
+
+def _process_all_btc_prices(trade_file: str, df_trades: pd.DataFrame):
+  start_time  = time.time()
+  
+  chunk_df    = _combine_and_convert_all_btc_prices()  
+  stat        = _process_backtest_chunk(chunk_df, df_trades.copy())
+  file_prefix = _generate_output_file_prefix(ALL_BTC_PRICE_FILES, trade_file, NO_CHUNK_INDEX_VALUE)
+  _output_to_files(stat, file_prefix)
+  end_time    = time.time()
+
+  print(f'      Processed chunk {NO_CHUNK_INDEX_VALUE} in {end_time - start_time} seconds.')
+  
+
+
+
+
+def _process_backtest(price_file: str, trade_file: str, chunk_size: int, do_all_btc_prices: bool):
+  price_file_msg = ALL_BTC_PRICE_FILES if do_all_btc_prices else price_file
+
+  print(f'Processing backtest using price file "{price_file_msg}" and trade file "{trade_file}"...')
   df_trades = _read_trade_file(trade_file)
 
-  if chunk_size == NO_CHUNK_SIZE_VALUE:
+  if do_all_btc_prices:
+    _process_all_btc_prices(trade_file, df_trades)
+    
+  elif chunk_size == NO_CHUNK_SIZE_VALUE:
     chunk_df = pd.read_csv(price_file)
     _process_one_chunk(chunk_df, price_file, trade_file, NO_CHUNK_INDEX_VALUE, df_trades) 
   else:
@@ -196,12 +233,14 @@ def _process_backtest(price_file: str, trade_file: str, chunk_size: int):
 
 if __name__ == '__main__':  
   parser = argparse.ArgumentParser(description="Process price and trade files.")
-  parser.add_argument("--price_file", default=DEFAULT_PRICE_FILE, help=f"Path to price file (default: {DEFAULT_PRICE_FILE})")
-  parser.add_argument("--trade_file", default=DEFAULT_TRADE_FILE, help=f"Path to trade file (default: {DEFAULT_TRADE_FILE})")
-  parser.add_argument("--chunk_size", default=DEFAULT_CHUNK_SIZE, help=f"Chunk size (default: {DEFAULT_CHUNK_SIZE}, use -1 to process entire file)")
+  parser.add_argument("--price_file"      , default=DEFAULT_PRICE_FILE, help=f"Path to price file (default: {DEFAULT_PRICE_FILE})")
+  parser.add_argument("--trade_file"      , default=DEFAULT_TRADE_FILE, help=f"Path to trade file (default: {DEFAULT_TRADE_FILE})")
+  parser.add_argument("--chunk_size"      , default=DEFAULT_CHUNK_SIZE, help=f"Chunk size (default: {DEFAULT_CHUNK_SIZE}, use -1 to process entire file)")
+
+  parser.add_argument("--all_btc_prices"  , default=0                 , help=f"1: combine the two hardcoded BTC price files (default: 0) and do nochunk")
   args = parser.parse_args()
 
   price_file_path = os.path.join(os.getcwd(), DATA_DIR, args.price_file)
   trade_file_path = os.path.join(os.getcwd(), DATA_DIR, args.trade_file)
 
-  _process_backtest(price_file_path, trade_file_path, int(args.chunk_size))
+  _process_backtest(price_file_path, trade_file_path, int(args.chunk_size), bool(args.all_btc_prices))
